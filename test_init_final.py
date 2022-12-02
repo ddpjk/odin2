e68ea947056692b0388fa8f016fd58a0148538f2
ddpjk <59241401+ddpjk@users.noreply.github.com>
2022-12-03 01:11:19 +0900

    Update test_init_final.py

--- test_init_final.py
+++ test_init_final.py
@@ -1153,16 +1153,16 @@ class taskCog(commands.Cog):
 						bossTimeString[i] = '99:99:99'
 						bossDateString[i] = '9999-99-99'
 						bossTime[i] = now+datetime.timedelta(days=365)
-						cut_message =  await ctx.send(f'"```" {bossData[i][0]} {bossData[i][4]} "\n" {bossData[i][0]} 컷 >> "⚔️" << 클릭 "```"')
 						if bossData[i][6] != '' :
 							embed = discord.Embed(
 									description= "```" + bossData[i][0] + bossData[i][4] + '\n<' + bossData[i][6] + '>```' ,
 									color=0x00ff00
 									)
 						else :
-							await ctx.send(f'"```" {bossData[i][0]} {bossData[i][4]} "\n" {bossData[i][0]} 컷 >> "⚔️" << 클릭 "```"')
-							def reaction_check(reaction, user):
-								return	(reaction.message.id == cut_message.id) and (user.id == ctx.author.id) and (str(reaction) in emoji_list)
+							embed = discord.Embed(
+									description= "```" + bossData[i][0] + bossData[i][4] + "\n" + bossData[i][0] + " 컷 >> " + "⚔️" + " << 클릭" + "```" ,
+									color=0x00ff00
+									)
 							if str(reaction) == "⚔️":
 								return ctx.send.message('aa')
 														
