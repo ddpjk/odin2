e530fcf502e9841f6b724874bd7a27bb23b43e12
ddpjk <59241401+ddpjk@users.noreply.github.com>
2022-12-03 00:14:44 +0900

    Update test_init_final.py

--- test_init_final.py
+++ test_init_final.py
@@ -1153,13 +1153,15 @@ class taskCog(commands.Cog):
 						bossTimeString[i] = '99:99:99'
 						bossDateString[i] = '9999-99-99'
 						bossTime[i] = now+datetime.timedelta(days=365)
+						cut_message =  await ctx.send(f'"```" + bossData[i][0] + bossData[i][4] + "```" + '\n' + bossData[i][0] + ' ' + '컷' + '>>' + ' ' + '⚔️' + ' ' + '<< 클릭'')
 						if bossData[i][6] != '' :
 							embed = discord.Embed(
 									description= "```" + bossData[i][0] + bossData[i][4] + '\n<' + bossData[i][6] + '>```' ,
 									color=0x00ff00
 									)
 						else :
-							return await ctx.send(f'"```" + bossData[i][0] + bossData[i][4] + "```" + '\n' + bossData[i][0] + ' ' + '컷' + '>>' + ' ' + '⚔️' + ' ' + '<< 클릭'')
+							def reaction_check(reaction, user):
+				return 				(reaction.message.id == cut_message.id) and (user.id == ctx.author.id) and (str(reaction) in emoji_list)
 							if str(reaction) == "⚔️":
 								print('aa')
 														
